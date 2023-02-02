501f2df203d3ac30d0d1b9748350d1c06edd2718
Arteminariy <artemborisoff.ab@gmail.com>
2023-02-02 15:23:54 +0300

    component gen templates

---
+++ client/templates/default/TemplateName.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TemplateName.module.css';
+
+interface TemplateNameProps { }
+
+const TemplateName = ({ }: TemplateNameProps) => (
+	<div className='templateName'>
+		TemplateName Component
+	</div>
+);
+
+export default TemplateName;
