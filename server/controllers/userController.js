74dc6842d015bbc8a81d71e95c6598c228faac49
Arteminariy <artemborisoff.ab@gmail.com>
2023-02-02 03:13:17 +0300

    reg, login, auth done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,17 +1,45 @@
 const ApiError = require('../error/ApiError')
-class userController {
-    async registration(req, res) {
+const bcrypt = require('bcrypt')
+const {User, Basket} = require('../models/models')
+const jwt = require('jsonwebtoken')
 
+const generateJWT = (id, login, role) => {
+    return jwt.sign(
+        {id, login, role}, 
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
+
+class userController {
+    async registration(req, res, next) {
+        const {login, password, role} = req.body
+        if(!login || !password) {
+            return next(ApiError.badRequest('Некорректный логин или пароль'))
+        }
+        const candidate = await User.findOne({where: {login}})
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь с таким логином уже существует'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({login, role, password: hashPassword})
+        const basket = await Basket.create({userId: user.id})
+        const token = generateJWT(user.id, user.login, user.role)
+        return res.json({token})
     }
-    async login(req, res) {
-        
+    async login(req, res, next) {
+        const {login, password} = req.body
+        const user = await User.findOne({where: {login}})
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!user || !comparePassword) {
+            return next(ApiError.internal('Неверный логин или пароль'))
+        }
+        const token = generateJWT(user.id, user.login, user.role)
+        return res.json({token})
     }
     async checkAuth(req, res, next) {
-        const {id} = req.query
-        if (!id) {
-            return next(ApiError.badRequest('Не задан id'))
-        }
-        res.json(id)
+        const token = generateJWT(req.user.id, req.user.login, req.user.role)
+        res.json({token})
     }
 }
 
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const userController = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
-router.get('/auth', userController.checkAuth)
+router.get('/auth', authMiddleware, userController.checkAuth)
 
 module.exports = router
