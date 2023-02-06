04bef2e12855428a4d55ae395bb18c6be8862d3b
Arteminariy <artemborisoff.ab@gmail.com>
2023-02-06 21:48:39 +0300

    no warn

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import store from './store'
+// import store from './store'
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
