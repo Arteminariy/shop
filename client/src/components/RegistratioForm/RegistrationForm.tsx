a56b4a20007625ff07979ee375af28c0225e21b1
Arteminariy <artemborisoff.ab@gmail.com>
2023-02-07 00:38:02 +0300

    n!

--- client/src/components/RegistratioForm/RegistrationForm.tsx
+++ client/src/components/RegistratioForm/RegistrationForm.tsx
@@ -10,7 +10,7 @@ export interface IRegistratioFormProps {
 
 const RegistratioForm: FC<IRegistratioFormProps> = ({}) => {
 	return (
-		<div className='registratioт-form'>
+		<div className='registration-form'>
 			<Input placeholder='Login'></Input>
 			<Input placeholder='Password' type='password'></Input>
 			<Button variant='outlined'>Войти</Button>
