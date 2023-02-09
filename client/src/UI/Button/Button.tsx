228ac7143c11ed8c3b9c9adb7e88ed2cb8eda02b
Arteminariy <artemborisoff.ab@gmail.com>
2023-02-09 20:13:43 +0300

    prettier

--- client/src/UI/Button/Button.tsx
+++ client/src/UI/Button/Button.tsx
@@ -1,20 +1,23 @@
-import React, { FC } from 'react';
-import './Button.css';
+import React, { FC } from "react";
+import "./Button.css";
 
 export interface IButtonProps {
-	children: JSX.Element | JSX.Element[] | string,
-	variant: ButtonStyleVariant,
-	click?: () => void
+    children: JSX.Element | JSX.Element[] | string;
+    variant: ButtonStyleVariant;
+    click?: () => void;
 }
-export type ButtonStyleVariant = 'filled' | 'outlined'
+export type ButtonStyleVariant = "filled" | "outlined";
 
 const Button: FC<IButtonProps> = ({ children, variant, click }) => (
-	<button
-		onClick={click}
-		className={variant === 'outlined' ? 'button outlined-btn' : 'button filled-btn'}
-		data-testid="Button">
-		{children}
-	</button>
+    <button
+        onClick={click}
+        className={
+            variant === "outlined" ? "button outlined-btn" : "button filled-btn"
+        }
+        data-testid="Button"
+    >
+        {children}
+    </button>
 );
 
 export default Button;
