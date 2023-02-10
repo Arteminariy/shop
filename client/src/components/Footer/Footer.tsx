7eff93742d06af08a22892466117bc553fe6aa51
Arteminariy <artemborisoff.ab@gmail.com>
2023-02-10 19:55:43 +0300

    Я заебался с этим футером

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,14 +1,46 @@
-import React, { FC } from 'react';
-import './Footer.css';
+import React, { FC } from "react";
+import "./Footer.css";
+import { Link } from "react-router-dom";
+import { Col, Row } from "antd";
 
-export interface IFooterProps { }
+export interface IFooterProps {
+    tabs: IFooterTabs[];
+}
 
-const Footer: FC<IFooterProps> = ({ }) => {
-	return (
-		<div className='footer'>
-			Footer Component
-		</div>
-	)
+export interface IFooterTabs {
+    tab: IFooterTab[];
+}
+export interface IFooterTab {
+    text: string;
+    link: string;
+}
+
+const Footer: FC<IFooterProps> = ({ tabs }) => {
+    return (
+        <footer>
+            <div className="footer-content">
+                <Row gutter={[16, 0]}>
+                    {tabs.map((el) => {
+                        return (
+                            <>
+                                {el.tab.map((tabEl) => {
+                                    return (
+                                        <Col span={6}>
+                                            <Link to={tabEl.link}>
+											<div className="footer-tab-text">
+                                                {tabEl.text}
+											</div>
+                                            </Link>
+                                        </Col>
+                                    );
+                                })}
+                            </>
+                        );
+                    })}
+                </Row>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
