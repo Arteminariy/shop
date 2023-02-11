8825d1f25d8da787f053b9050e68c11840aea5d2
Arteminariy <artemborisoff.ab@gmail.com>
2023-02-11 06:51:25 +0300

    Header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,32 +1,45 @@
-import React, { FC } from 'react';
-import './Header.css';
-import { Link } from 'react-router-dom'
+import React, { FC } from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import user_icon from "../../assets/User_Empty.png";
 
 export interface IHeaderProps {
-	tabs: IHeaderTab[]
+    tabs: IHeaderTab[];
 }
 
 export interface IHeaderTab {
-	title: string,
-	link: string
+    title: string;
+    link: string;
 }
 
 const Header: FC<IHeaderProps> = ({ tabs }) => {
-	return (
-		<header>
-			<div className="header-content">
-				{tabs.map(tab => {
-					return (
-						<div className="header-tab">
-							<Link to={tab.link} className="header-tab-text">
-								{tab.title}
-							</Link>
-						</div>
-					)
-				})}
-			</div>
-		</header>
-	)
+    return (
+        <header>
+            <div className="header-content">
+                <div className="header-top">
+                    <div className="header-logo">Shine Bright</div>
+                    <div className="header-buttons">
+                        <Link to="/">
+                            <div className="header-icon">
+                                <img src={user_icon} alt="Профиль" />
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+                <div className="header-tabs">
+                    {tabs.map((tab) => {
+                        return (
+                            <div className="header-tab">
+                                <Link to={tab.link} className="header-tab-text">
+                                    {tab.title}
+                                </Link>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
