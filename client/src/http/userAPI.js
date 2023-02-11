f6655cea8798ae16ce56596de22a5101396342d0
Arteminariy <artemborisoff.ab@gmail.com>
2023-02-11 07:16:28 +0300

    Reg api frontend

---
+++ client/src/http/userAPI.js
@@ -0,0 +1,17 @@
+import { $authHost, $host } from ".";
+import jwt from 'jwt-decode'
+
+export const registration = async (login, password) => {
+    const {data} = await $host.post('api/user/registration', {email, password})
+    return jwt.decode(data.token)
+}
+
+export const login = async (login, password) => {
+    const {data} = await $host.post('api/user/login', {email, password})
+    return jwt.decode(data.token)
+}
+
+export const check = async (login, password) => {
+    const response = await $host.post('api/user/auth')
+    return response
+}
