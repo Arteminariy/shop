21340294857d26ded4a46b66464874d710447cbe
Arteminariy <artemborisoff.ab@gmail.com>
2023-02-12 20:44:58 +0300

    перепроверил работу бэка

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -1,65 +1,61 @@
-const { Brand } = require('../models/models')
-const ApiError = require('../error/ApiError')
+const { Brand } = require("../models/models");
+const ApiError = require("../error/ApiError");
 class brandController {
     async create(req, res, next) {
         try {
-            const { name } = req.body
+            const { name } = req.body;
 
             if (name) {
-                const brand = await Brand.create({ name })
-                return next(res.status(201).json(brand))
-            }
-            else {
-                return next(ApiError.badRequest(error.messege))
+                const brand = await Brand.create({ name });
+                return next(res.status(201).json(brand));
+            } else {
+                return next(ApiError.badRequest(error.messege));
             }
         } catch (error) {
-            next(ApiError.internal(error.messege))
+            next(ApiError.internal(error.messege));
         }
     }
     async getAll(req, res, next) {
         try {
-            const brands = await Brand.findAll()
-            return res.status(200).json(brands)
+            const brands = await Brand.findAll();
+            return res.status(200).json(brands);
         } catch (error) {
-            next(ApiError.internal(error.messege))
+            next(ApiError.internal(error.messege));
         }
     }
     async update(req, res, next) {
         try {
-            let { name } = req.body
-            const { id } = req.params
-            const brand = await Brand.findOne({ where: { id: id } })
+            let { name } = req.body;
+            const { id } = req.params;
+            const brand = await Brand.findOne({ where: { id: id } });
             if (brand && name) {
-                await Brand.update({ name }, { where: { id: id } })
-                return res.json({ message: 'Товар изменён' })
+                await Brand.update({ name }, { where: { id: id } });
+                return res.json({ message: `Брэнд №${id} изменён` });
             }
             if (!brand) {
-                return next(ApiError.badRequest(error.message))
+                return next(ApiError.badRequest(error.message));
             }
             if (!name) {
-                return next(ApiError.badRequest(error.message))
+                return next(ApiError.badRequest(error.message));
             }
         } catch (error) {
-            next(ApiError.internal(error.messege))
+            next(ApiError.internal(error.messege));
         }
     }
     async remove(req, res, next) {
         try {
-            const { id } = req.params
-            const brand = await Brand.findOne({ where: { id: id } })
+            const { id } = req.params;
+            const brand = await Brand.findOne({ where: { id: id } });
             if (brand) {
-                await Brand.destroy({ where: { id: id } })
-                res.json({
-                    message: `Уволен id:${id}`
-                })
-            }
-            else {
-                next(ApiError.notFound(error.message))
+                await Brand.destroy({ where: { id: id } });
+                res.json({ message: `Уволен id:${id}` });
+            } else {
+                next(ApiError.notFound(error.message));
             }
         } catch (error) {
-            next(ApiError.internal(error.messege))
+            next(ApiError.internal(error.messege));
         }
     }
 }
 
-module.exports = new brandController()
+module.exports = new brandController();
--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,33 +1,64 @@
-const {Type} = require('../models/models')
-const ApiError = require('../error/ApiError')
+const { Type } = require("../models/models");
+const ApiError = require("../error/ApiError");
 
 class typeController {
     async create(req, res) {
-        const {name} = req.body
-        const type = await Type.create({name})
-        return res.json(type)
+        try {
+            const { name } = req.body;
+
+            if (name) {
+                const type = await Type.create({ name });
+                return next(res.status(201).json(type));
+            } else {
+                return next(ApiError.badRequest(error.messege));
+            }
+        } catch (error) {
+            next(ApiError.internal(error.message));
+        }
     }
     async getAll(req, res) {
-        const types = await Type.findAll()
-        return res.json(types)
+        try {
+            const types = await Type.findAll();
+            return res.json(types);
+        } catch (error) {
+            next(ApiError.internal(error.message));
+        }
     }
     async update(req, res) {
         try {
-            
+            let { name } = req.body;
+            const { id } = req.params;
+            const type = await Type.findOne({ where: { id: id } });
+            if (type && name) {
+                await Type.update({ name }, { where: { id: id } });
+                return res.json({ message: `Тип №${id} изменён` });
+            }
+            if (!type) {
+                return next(ApiError.badRequest(error.message));
+            }
+            if (!name) {
+                return next(ApiError.badRequest(error.message));
+            }
         } catch (error) {
-            next(ApiError.internal(error.message))
+            next(ApiError.internal(error.messege));
         }
     }
     async remove(req, res) {
         try {
-
+            const { id } = req.params;
+            const type = await Type.findOne({ where: { id: id } });
+            if (type) {
+                await Type.destroy({ where: { id: id } });
+                res.json({
+                    message: `Уволен id:${id}`,
+                });
+            } else {
+                next(ApiError.notFound(error.message));
+            }
         } catch (error) {
-            console.log(error)
-            res.status(500).json({
-                message: 'Не удалось удалить товар'
-            })
+            next(ApiError.internal(error.messege));
         }
     }
 }
 
-module.exports = new typeController()
+module.exports = new typeController();
