7f0df97f06efe93b035cb3371de7d70e55a471a7
Arteminariy <artemborisoff.ab@gmail.com>
2023-02-12 21:13:25 +0300

    validation

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -2,10 +2,11 @@ const Router = require('express')
 const router = new Router()
 const typeController = require('../controllers/typeController')
 const checkRole = require('../middleware/checkRoleMiddleware')
+const { typeValidator } = require('../validators')
 
-router.post('/', checkRole('ADMIN'), typeController.create)
+router.post('/', checkRole('ADMIN'), typeValidator, typeController.create)
 router.get('/', typeController.getAll)
-router.patch('/:id', checkRole('ADMIN'), typeController.update)
-router.delete('/:id', checkRole('ADMIN'), typeController.remove)
+router.patch('/:id', checkRole('ADMIN'), typeValidator, typeController.update)
+router.delete('/:id', checkRole('ADMIN'), typeValidator, typeController.remove)
 
 module.exports = router
