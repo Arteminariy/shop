c817703c05ae18c2f18fbe4cab1a7f846e65f64c
Arteminariy <artemborisoff.ab@gmail.com>
2023-02-14 17:27:35 +0300

    lint server

--- server/index.js
+++ server/index.js
@@ -1,33 +1,35 @@
-require('dotenv').config()
-const express = require('express')
-const sequelize = require('./db')
-const models = require('./models/models')
-const fileUpload = require('express-fileupload')
-const cors = require('cors')
-const router = require('./routes/index')
-const errHandler = require('./middleware/ErrorHandlingMiddleware')
-const path = require('path')
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
+const models = require("./models/models");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const router = require("./routes/index");
+const errHandler = require("./middleware/ErrorHandlingMiddleware");
+const path = require("path");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'static')))
-app.use(fileUpload({}))
-app.use('/api', router)
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
+app.use("/api", router);
 
 //Обработчик ошибок (Должен стоять в конце)
-app.use(errHandler)
+app.use(errHandler);
 
 const start = async () => {
     try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`))
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () =>
+            console.log(`Server has been started on port ${PORT}`)
+        );
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
+};
 
-start()
+start();
