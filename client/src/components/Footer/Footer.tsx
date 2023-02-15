1276d0a5280ada733d58c444b8a60d88b44501d9
Arteminariy <artemborisoff.ab@gmail.com>
2023-02-15 22:11:28 +0300

    footer upd

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 export interface IFooterProps {
@@ -12,26 +13,26 @@ const Footer: FC<IFooterProps> = () => {
 			<div className="footer-content">
 				<div className="footer-tab">
 					<h4 className="footer-tab-heading">Познакомиться с Shine Bright</h4>
-					<p className="footer-tab-text">Макияж</p>
-					<p className="footer-tab-text">Уход за кожей</p>
-					<p className="footer-tab-text">Уход за телом</p>
-					<p className="footer-tab-text">Парфюмерия</p>
+					<Link to={'/'} className="footer-tab-text">Макияж</Link>
+					<Link to={'/'} className="footer-tab-text">Уход за кожей</Link>
+					<Link to={'/'} className="footer-tab-text">Уход за телом</Link>
+					<Link to={'/'} className="footer-tab-text">Парфюмерия</Link>
 				</div>
 				<div className="footer-tab">
 					<h4 className="footer-tab-heading">Онлайн услуги</h4>
-					<p className="footer-tab-text">Учётная запись</p>
-					<p className="footer-tab-text">Сервис</p>
+					<Link to={'/'} className="footer-tab-text">Учётная запись</Link>
+					<Link to={'/'} className="footer-tab-text">Сервис</Link>
 				</div>
 				<div className="footer-tab">
 					<h4 className="footer-tab-heading">Каталог</h4>
-					<p className="footer-tab-text">Помады</p>
-					<p className="footer-tab-text">Крем</p>
-					<p className="footer-tab-text">Парфюмерия</p>
-					<p className="footer-tab-text">Ароматы для дома</p>
+					<Link to={'/'} className="footer-tab-text">Помады</Link>
+					<Link to={'/'} className="footer-tab-text">Крем</Link>
+					<Link to={'/'} className="footer-tab-text">Парфюмерия</Link>
+					<Link to={'/'} className="footer-tab-text">Ароматы для дома</Link>
 				</div>
 				<div className="footer-tab">
 					<h4 className="footer-tab-heading">О нас</h4>
-					<p className="footer-tab-text">История создания</p>
+					<Link to={'/'} className="footer-tab-text">История создания</Link>
 				</div>
 			</div>
 		</footer>
